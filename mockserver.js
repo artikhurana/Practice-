eb320d466121d4ba6f9c3e24716edf64aa714e60
Arti <akhurana@stonewain.com>
2016-11-05 14:53:01 +0530

    Added sample form in server.

---
+++ mockserver.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+    app = express(),
+    fs = require('fs'),
+    bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname));
+
+app.post('/mock-practice', function (req, res) {
+
+    console.log(JSON.stringify(req.body));
+    res.json(req.body);
+});
+
+app.listen(5000, function () {
+    console.log('Practice app listening on port 4000!');
+});
